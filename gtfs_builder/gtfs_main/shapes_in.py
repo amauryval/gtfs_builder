367f6b80c9873485d0db1639ba6b95d20e675d62
amaury5432 <amauryval@gmail.com>
2019-11-19 23:44:01 +0100

    add (shapes_in.py)

---
+++ gtfs_builder/gtfs_main/shapes_in.py
@@ -0,0 +1,23 @@
+from gtfs_builder.gtfs_core.core import OpenGtfs
+
+
+class Shapes(OpenGtfs):
+
+    def __init__(self, input_file="shapes.txt"):
+
+        super(Shapes, self).__init__(input_file)
+        self.__build_vehicule_trace()
+
+    def __build_vehicule_trace(self):
+        self._input_data = self.gdf_from_df_long_lat(
+            self._input_data,
+            "shape_pt_lon",
+            "shape_pt_lat"
+        )
+
+        self._input_data = self.group_by_id_from_point_to_create_linestring(
+            self._input_data,
+            "shape_id",
+            "shape_pt_sequence"
+        )
+
