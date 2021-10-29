59fa117bbbc78b7e8d5b47d582d0298fd7b181dc
amaury5432 <amauryval@gmail.com>
2021-10-30 00:46:35 +0200

    upd (routes_in) transport modes filter

--- gtfs_builder/gtfs_cp_inputs/routes_in.py
+++ gtfs_builder/gtfs_cp_inputs/routes_in.py
@@ -1,3 +1,6 @@
+from typing import List
+from typing import Optional
+
 from gtfs_builder.gtfs_core.core import OpenGtfs
 
 
@@ -14,14 +17,20 @@ class Routes(OpenGtfs):
         "7": "funicular"
     }
 
-    def __init__(self, geo_tools_core, input_file="routes.txt"):
+    def __init__(self, geo_tools_core, transport_modes: Optional[List[str]] = None, input_file="routes.txt"):
 
         super(Routes, self).__init__(geo_tools_core, input_file)
         self.__remap_route_type()
+        if transport_modes is not None:
+            self.__filter_by_route_types(transport_modes)
 
     def __remap_route_type(self):
         self._input_data.replace(
             {"route_type": self._ROUTE_TYPE_MAPPING},
             inplace=True
         )
+
+    def __filter_by_route_types(self, transport_modes: List[str]):
+        self._input_data = self._input_data.loc[self._input_data["route_type"].isin(transport_modes)]
+
 
