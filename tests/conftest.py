e991b4f327fc7843c136e414bab0354fba8e1e20
amaury5432 <amauryval@gmail.com>
2021-11-01 21:46:32 +0100

    add (tests) routes tests

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+from gtfs_builder.gtfs_app.routes import gtfs_routes
+
+from flask import Flask
+from dotenv import load_dotenv
+
+from spatialpandas import io
+
+load_dotenv(".gtfs.env")
+
+
+@pytest.fixture(scope="session")
+def flask_client():
+
+    session = io.read_parquet_dask("../stops.parq")
+
+    app = Flask(__name__)
+    app.register_blueprint(gtfs_routes(session, None, "parquet"), url_prefix="")
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+    app.config['JSON_SORT_KEYS'] = False
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = True
+
+    gtfs_api_client = app.test_client()
+
+    yield gtfs_api_client
