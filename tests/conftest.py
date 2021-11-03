7857748445903f3b50eff0302cab8221675e3578
amaury5432 <amauryval@gmail.com>
2021-11-03 21:10:50 +0100

    upd (tests) less point created

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,10 +15,10 @@ load_dotenv(".gtfs.env")
 @pytest.fixture(scope="session")
 def flask_client():
 
-    session = io.read_parquet_dask("../stops.parq")
+    session = io.read_parquet("fake_moving_stops.parq")
 
     app = Flask(__name__)
-    app.register_blueprint(gtfs_routes(session, None, "parquet"), url_prefix="")
+    app.register_blueprint(gtfs_routes(session), url_prefix="")
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
     app.config['JSON_SORT_KEYS'] = False
     app.config['DEBUG'] = True
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,11 +10,11 @@ def test_range_dates_route(flask_client):
 
 
 def test_nodes_by_date_route(flask_client):
-    response = flask_client.get("/nodes_by_date?current_date=2019-11-15 10:01:00", content_type="html/text")
+    response = flask_client.get("/nodes_by_date?current_date=2007-01-01 09:07:20", content_type="html/text")
     assert response.status_code == 200
 
     output_data = response.json
     assert len(output_data["data_geojson"]) > 0
     assert isinstance(output_data["data_geojson"], list)
-    assert set(list(output_data["data_geojson"][0].keys())) == set(["stop_code", "x", "y", "line_name_short"])
+    assert set(list(output_data["data_geojson"][0].keys())) == set(["stop_code", "x", "y", "route_short_name"])
 
