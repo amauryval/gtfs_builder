25b59b39dcebb24ac8a341375af6fb3fd060bd11
amaury5432 <amauryval@gmail.com>
2022-06-17 19:58:00 +0200

    upd (trips_in.py) add filter func for service_id

--- gtfs_builder/inputs_builders/trips_in.py
+++ gtfs_builder/inputs_builders/trips_in.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from gtfs_builder.core.core import OpenGtfs
 
 
@@ -9,13 +11,16 @@ class Trips(OpenGtfs):
         "1": "forth"
     }
 
-    def __init__(self, core, input_file: str = "trips.txt") -> None:
+    def __init__(self, core, input_file: str = "trips.txt", service_id_filtering: Optional[str] = None) -> None:
 
         super(Trips, self).__init__(core, core.path_data, input_file)
         self.__clean_direction_id()
 
         if self.is_df_empty(self._input_data):
             raise ValueError(f"'{input_file}' is empty")
+
+        if service_id_filtering is not None:
+            self.__filter_by_service_id(service_id_filtering)
 
     def __clean_direction_id(self) -> None:
         if self._DIRECTION_FIELD_NAME not in self._input_data.columns.to_list():
@@ -26,3 +31,6 @@ class Trips(OpenGtfs):
             "3" if row not in self._DIRECTON_MAPPING.keys() else row
             for row in self._input_data[self._DIRECTION_FIELD_NAME]
         ]
+
+    def __filter_by_service_id(self, service_id):
+        self._input_data = self._input_data.loc[self._input_data["service_id"].isin(service_id)]
